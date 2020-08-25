fc6a070fabe6f692471c3bd107406b5e2a93be1a
ByeonGooHoon <dksekfldks65@naver.com>
2020-08-25 21:37:16 +0900

    IoC컨테이너 : Message Resource를 이용한 다국어 처리

---
+++ spring-main-tech03/src/main/java/com/hoon/SpringMainTech03Application.java
@@ -0,0 +1,27 @@
+package com.hoon;
+
+import lombok.var;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+@SpringBootApplication
+public class SpringMainTech03Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMainTech03Application.class, args);
+    }
+
+    @Bean
+    public MessageSource messageSource(){       //릴로딩 기능이 있는 메시지 소스 사용하기
+        var messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setCacheSeconds(3); //최대 3초동안 캐싱
+        return messageSource;
+    }
+
+}
---
+++ spring-main-tech03/src/test/java/com/hoon/SpringMainTech03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMainTech03ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
