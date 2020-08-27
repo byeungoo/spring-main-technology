61c342cc4f5f22f01ca0f14290f95aaabe042b14
ByeonGooHoon <dksekfldks65@naver.com>
2020-08-27 22:57:45 +0900

    Resource 추상화 예제 진행

--- spring-main-tech03/src/main/java/com/hoon/ResourceAppRunner.java
+++ spring-main-tech03/src/main/java/com/hoon/ResourceAppRunner.java
@@ -3,6 +3,7 @@ package com.hoon;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.ApplicationContext;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
@@ -13,15 +14,23 @@ import java.nio.file.Files;
 public class ResourceAppRunner implements ApplicationRunner {
 
     @Autowired
-    ResourceLoader resourceLoader;
+    ApplicationContext resourceLoader;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        System.out.println(resourceLoader.getClass()); //WebApplicationContext
+
         Resource resource = resourceLoader.getResource("classpath:test.txt"); // target/classes가 classpath root임. 이 아래로 파일을 찾게됩니다.
+        System.out.println(resource.getClass()); //접두어 classpath를 썼기 때문에 ClassPathResource가 나옴.
         System.out.println(resource.exists());
         System.out.println(resource.getDescription());
 
-        Resource resource2 = resourceLoader.getResource("classpath:/static/test2.txt");
+        Resource resource2 = resourceLoader.getResource("/static/test2.txt");
+        System.out.println(resource2.getClass()); //접두어를 안붙였기 때문에 기본적으로 ServletContextResource가 나옴
         System.out.println(resource2.exists());
+
+        Resource resource3 = resourceLoader.getResource("test3.txt");
+        System.out.println(resource3.getClass()); //접두어를 안붙였기 때문에 기본적으로 ServletContextResource가 나옴
+        System.out.println(resource3.exists());
     }
 }
