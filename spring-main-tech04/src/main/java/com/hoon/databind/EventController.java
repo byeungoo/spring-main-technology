d287a7c5d39dfc955baab67b1069e228c68f7fdb
ByeonGooHoon <dksekfldks65@naver.com>
2020-09-01 00:05:25 +0900

    데이터 바인딩 추상화 테스트 코드 작성

--- spring-main-tech04/src/main/java/com/hoon/databind/EventController.java
+++ spring-main-tech04/src/main/java/com/hoon/databind/EventController.java
@@ -1,11 +1,18 @@
 package com.hoon.databind;
 
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EventController {
+
+    @InitBinder //컨트롤러에서 사용할 바인더 등록
+    public void init(WebDataBinder webDataBinder){  //데이터 바인더의 구현체 중 하나
+        webDataBinder.registerCustomEditor(Event.class, new EventEditor());
+    }
 
     @GetMapping("/event/{event}")
     public String getEvent(@PathVariable Event event){
